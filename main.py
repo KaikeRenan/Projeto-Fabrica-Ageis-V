c0ceecb40a0f764fa50b346194a5f41f5a6ce166
Gustavo Marques <gustavomqm@hotmail.com>
2026-04-13 17:40:04 -0300

    adicionado na main a rota do Get Valuation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from controllers.user_controller import router
+from controllers.finance_controller import router as finance_router
 
 app = FastAPI()
 
 app.include_router(router)
-
+app.include_router(finance_router)
 # @app.get("/")
 # async def root():
 #     return {"message": "Hello World"}
